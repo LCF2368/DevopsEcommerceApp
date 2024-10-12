a2c92eeac0ca2146d23b17734a8dcfd0d28e1af9
LCF2368 <abhishekreddy2048@gmail.com>
2024-10-12 11:47:23 -0400

    added env file
    added env file to the project

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv()
 
 # MongoDB connection
-client = MongoClient("mongodb+srv://nkreddy4312:pZxUG3qxFycc3RpJ@cluster0.spq8j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = client['shop_db']
+mongo_username = os.getenv('MONGODB_USERNAME')
+mongo_password = os.getenv('MONGODB_PASSWORD')
+mongo_uri = os.getenv('MONGODB_URI')
+
+# Connect to MongoDB Atlas
+client = MongoClient(mongo_uri)
+db = client.shop_db
 products_collection = db['products']
 
 @app.route('/')
